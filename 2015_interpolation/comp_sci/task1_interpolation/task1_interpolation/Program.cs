9ec72dbcfa0921085d91c17b0bba301b9b7ed37d
unknown <st032080@cr03429.ad.pu.ru>
2015-10-30 10:07:27 +0300

    task1 changes; checked

--- 2015_interpolation/comp_sci/task1_interpolation/task1_interpolation/Program.cs
+++ 2015_interpolation/comp_sci/task1_interpolation/task1_interpolation/Program.cs
@@ -20,18 +20,20 @@ namespace task1_interpolation
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var interval = new Interval(1, 100);
+            var interval = new Interval(-1, 1);
             double step = 0.01f;
 
             /* FOR f(x) */
-            Interpolator interpolator_f = new Interpolator(f, 2, interval);
+            Interpolator interpolator_f = new Interpolator(f, 20, interval, false);
+            Interpolator interpolator_f_eq = new Interpolator(f, 20, interval, true);
 
             var originalGraph_f = GraphGenerator.generateGraphForFunc("original", f, interval, step, Color.Red);
             var interpolatedGraph_f = GraphGenerator.generateGraphForFunc("interpolated", interpolator_f.lagrangePolynom, interval, step * 10, Color.Green);
+            var interpolatedGraph_f_eq = GraphGenerator.generateGraphForFunc("interpolated_eq", interpolator_f_eq.lagrangePolynom, interval, step * 10, Color.Blue);
 
 
             List<GraphData> graphsList_f = new List<GraphData>() {
-                originalGraph_f, interpolatedGraph_f
+                originalGraph_f, interpolatedGraph_f, interpolatedGraph_f_eq
             };
 
             Graph form_f = new Graph("Lagrange interpolation for f(x)", graphsList_f);
@@ -40,13 +42,15 @@ namespace task1_interpolation
 
 
             /* FOR h(x) */
-            Interpolator interpolator_h = new Interpolator(h, 2, interval);
+            Interpolator interpolator_h = new Interpolator(h, 8, interval, false);
+            Interpolator interpolator_h_eq = new Interpolator(h, 8, interval, true);
+
             var originalGraph_h= GraphGenerator.generateGraphForFunc("original", h, interval, step, Color.Red);
             var interpolatedGraph_h = GraphGenerator.generateGraphForFunc("interpolated", interpolator_h.lagrangePolynom, interval, step * 10, Color.Green);
-
+            var interpolatedGraph_h_eq = GraphGenerator.generateGraphForFunc("interpolated_eq", interpolator_h_eq.lagrangePolynom, interval, step * 10, Color.Blue);
 
             List<GraphData> graphsList_h = new List<GraphData>() {
-                originalGraph_h, interpolatedGraph_h
+                originalGraph_h, interpolatedGraph_h, interpolatedGraph_h_eq
             };
 
 
@@ -63,7 +67,7 @@ namespace task1_interpolation
 
         public static double f(double x)
         {
-            return x * x - 1 - Math.Log(x);
+            return x * x - Math.Log(x+2);
         }
 
         public static double h(double x) {

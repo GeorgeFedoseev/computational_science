301c5fc6fc81b14ba7e7ba423b12968abba3cf2d
George <spamgoga@gmail.com>
2015-11-18 21:06:42 +0400

    preparations

---
+++ 2015_approximation/2015_approximation/Approximator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _2015_approximation
+{
+
+    class Approximator
+    {
+        private Func<double, double> f;
+        private int nodes_count;
+        Interval interval;
+        bool equidistant;
+               
+
+        public Approximator(Func<double, double> _f, int _nodes_count, Interval _interval, bool _equidistant = true) {
+            init(_f, _nodes_count, _interval, _equidistant);
+        }
+
+
+        private void init(Func<double, double> _f, int _nodes_count,  Interval _interval, bool _equidistant = true){
+            f = _f;
+            nodes_count = _nodes_count;
+            interval = _interval;
+            equidistant = _equidistant;
+
+            var nodes = generateSpecialInterpolationNodes();
+
+          //  Console.WriteLine(string.Join(", ", nodes));            
+        }
+
+
+        public double lagrangePolynom(double x) {           
+
+            List<double> nodes;
+            if (equidistant)
+                nodes = generateEquidistantInterpolationNodes();
+            else
+                nodes = generateSpecialInterpolationNodes();
+
+
+            double result = 0;
+            for (int i = 0; i <= nodes_count; i++) {
+                double summand = 1;
+                for (int j = 0; j <= nodes_count; j++) {
+                    if (i != j)
+                        summand *= (x - nodes[j]) / (nodes[i] - nodes[j]);
+                }
+
+                summand *= f(nodes[i]);
+
+                result += summand;
+            }
+
+            return result;
+        }
+
+
+        private List<double> generateSpecialInterpolationNodes(){
+            List<double> nodes = new List<double>();
+            
+            for (var i = 0; i <= nodes_count; i++) {
+                nodes.Insert(0, 
+                        (double)0.5 * 
+                            (interval.length * 
+                                (double)Math.Cos((2 * (double)i + 1) / (2 * (nodes_count + 1)) * (double)Math.PI)
+                                + interval.from + interval.to)
+                    );
+            }
+
+            return nodes;
+        }
+
+        private List<double> generateEquidistantInterpolationNodes()
+        {
+            List<double> nodes = new List<double>();
+
+            for (var i = 0; i <= nodes_count; i++)
+            {
+                nodes.Add(interval.from + (double)i * interval.length / nodes_count);
+            }
+
+            return nodes;
+        }
+
+    }
+}
---
+++ 2015_approximation/2015_approximation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2015_approximation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using ZedGraph;
+
+    static class Program
+    {
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var interval = new Interval(-1, 1);
+            double step = 0.01f;
+
+            /* FOR f(x) */
+            Approximator interpolator_f = new Approximator(f, 20, interval, false);
+            Approximator interpolator_f_eq = new Approximator(f, 20, interval, true);
+
+            var originalGraph_f = GraphGenerator.generateGraphForFunc("original", f, interval, step, Color.Red);
+            var interpolatedGraph_f = GraphGenerator.generateGraphForFunc("interpolated", interpolator_f.lagrangePolynom, interval, step * 10, Color.Green);
+            var interpolatedGraph_f_eq = GraphGenerator.generateGraphForFunc("interpolated_eq", interpolator_f_eq.lagrangePolynom, interval, step * 10, Color.Blue);
+
+
+            List<GraphData> graphsList_f = new List<GraphData>() {
+            originalGraph_f, interpolatedGraph_f, interpolatedGraph_f_eq
+        };
+
+            Graph form_f = new Graph("Lagrange interpolation for f(x)", graphsList_f);
+            form_f.Show();
+
+
+            Application.Run(form_f);
+
+            // dont exit app
+            // while (true) ;            
+        }
+
+
+        public static double f(double x)
+        {
+            return (x + 3) * Math.Cos(x);
+        }
+
+
+
+    }
+    
+
+}
